a24deb901a43fea8fbea31bc77435ee8d280180a
Christopher Greer <chrisfg.99@hotmail.com>
2022-07-01 21:09:44 +0100

    Add files via upload

---
+++ ass2.sql
@@ -0,0 +1,71 @@
+-- Answer to the 2nd Database Assignment 2019/20
+--
+-- CANDIDATE NUMBER 
+-- Please insert your candidate number in the line above.
+-- Do NOT remove ANY lines of this template.
+
+
+-- In each section below put your answer in a new line 
+-- BELOW the corresponding comment.
+-- Use ONE SQL statement ONLY per question.
+-- If you don’t answer a question just leave 
+-- the corresponding space blank. 
+-- Anything that does not run in SQL you MUST put in comments.
+-- Your code should never throw a syntax error.
+-- Questions with syntax errors will receive zero marks.
+
+-- DO NOT REMOVE ANY LINE FROM THIS FILE.
+
+-- START OF ASSIGNMENT CODE
+
+
+-- @@01
+
+
+ 
+-- @@02
+
+
+
+-- @@03
+
+
+
+-- @@04
+
+
+
+-- @@05
+
+
+
+-- @@06
+
+
+
+-- @@07
+
+
+
+-- @@08
+
+
+
+-- @@09
+
+
+
+-- @@10
+
+
+
+-- @@11
+
+
+
+-- @@12
+delimiter $$
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+
+-- END OF ASSIGNMENT CODE
